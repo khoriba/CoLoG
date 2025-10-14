89b830b66ad9282dcb663f282ff4f6d3689aa051
Kazuhiro Horiba <45022668+khoriba@users.noreply.github.com>
2025-10-14 22:00:19 +0900

    one-line-FASTA.pl

--- scripts/one-line-FASTA.pl
+++ scripts/one-line-FASTA.pl
@@ -1,26 +1,4 @@
 #!/usr/bin/perl
-#
-# new version! FASTAのフォーマットを一発で一行にするスクリプト。
-#  
-#　こんなやつが、
-# >E8Y3Q8Q03FMQXE
-# GTGATATATGTGGTGTGTGATGTGTAAGGTGTGTATGTG
-# TGTAATGTGTGCATAGTGTTTGTGGTGCATGTGTGTGTG
-# TGGTGTGT
-#
-# こんな風に！
-# >E8Y3Q8Q03FMQXE:GTGATATATGTGGTGTGTGATGTGTAAGGTGTGTATGTGTGTAATGTGTGCATAGTGTTTGTGGTGCATGTGTGTGTGTGGTGTG
-#
-#
-# nucleotide, amino acid両方のシークエンス配列に使用出来ます。
-#
-#
-# sekizuka@nih.go.jp
-#
-#
-
-
-
  
 while ($buffer = <>) {
     if ($buffer =~ /^>/) {
